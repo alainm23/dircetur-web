f950aa20efe4983920969b314407ff89a275874d
Alain M <alain23@protonmail.com>
2020-08-20 15:15:52 -0500

    *

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -71,8 +71,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  // imports: [RouterModule.forRoot(routes, { useHash: true })],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  // imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 
